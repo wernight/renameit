145d5b4ef88d106b0902a27d3806b9c999daceea
Werner Beroux <werner@beroux.com>
2008-06-05 16:05:53 +0000

    *Rename-It!* - Improved project compilation output. - Clean-up StdAfx.h

--- apps/Rename-It!/StdAfx.h
+++ apps/Rename-It!/StdAfx.h
@@ -20,15 +20,10 @@
 //      are changed infrequently
 //
 
-#if !defined(AFX_STDAFX_H__ECCA64B8_40F9_11D2_AD2F_00A0C9289E24__INCLUDED_)
-#define AFX_STDAFX_H__ECCA64B8_40F9_11D2_AD2F_00A0C9289E24__INCLUDED_
-
-#if _MSC_VER >= 1000
-# pragma once
-#endif // _MSC_VER >= 1000
+#pragma once
 
 #ifndef _SECURE_ATL
-#define _SECURE_ATL 1
+# define _SECURE_ATL 1
 #endif
 
 #ifndef VC_EXTRALEAN
@@ -53,21 +48,21 @@
 
 #else						// Windows 2000-XP
 
-#ifndef WINVER				// Allow use of features specific to Windows XP or later.
-#define WINVER 0x0501		// Change this to the appropriate value to target other versions of Windows.
-#endif
+# ifndef WINVER				// Allow use of features specific to Windows XP or later.
+#  define WINVER 0x0501		// Change this to the appropriate value to target other versions of Windows.
+# endif
 
-#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
-#define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
-#endif						
+# ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
+#  define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
+# endif						
 
-#ifndef _WIN32_WINDOWS		// Allow use of features specific to Windows 98 or later.
-#define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
-#endif
+# ifndef _WIN32_WINDOWS		// Allow use of features specific to Windows 98 or later.
+#  define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
+# endif
 
-#ifndef _WIN32_IE			// Allow use of features specific to IE 6.0 or later.
-#define _WIN32_IE 0x0600	// Change this to the appropriate value to target other versions of IE.
-#endif
+# ifndef _WIN32_IE			// Allow use of features specific to IE 6.0 or later.
+#  define _WIN32_IE 0x0600	// Change this to the appropriate value to target other versions of IE.
+# endif
 
 #endif
 
@@ -82,17 +77,13 @@
 
 #include <afxwin.h>         // MFC core and standard components
 #include <afxext.h>         // MFC extensions
-
-
 #include <afxdisp.h>        // MFC Automation classes
 
-
-
 #ifndef _AFX_NO_OLE_SUPPORT
-#include <afxdtctl.h>		// MFC support for Internet Explorer 4 Common Controls
+# include <afxdtctl.h>		// MFC support for Internet Explorer 4 Common Controls
 #endif
 #ifndef _AFX_NO_AFXCMN_SUPPORT
-#include <afxcmn.h>			// MFC support for Windows Common Controls
+# include <afxcmn.h>		// MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
 #include <afxcoll.h>
@@ -100,15 +91,15 @@
 #include <afxtempl.h>
 
 #ifdef _UNICODE
-#if defined _M_IX86
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
-#elif defined _M_IA64
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='ia64' publicKeyToken='6595b64144ccf1df' language='*'\"")
-#elif defined _M_X64
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
-#else
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-#endif
+# if defined _M_IX86
+#  pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+# elif defined _M_IA64
+#  pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='ia64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+# elif defined _M_X64
+#  pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+# else
+#  pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+# endif
 #endif
 
 // STL
@@ -134,6 +125,3 @@ using namespace boost;
 inline long int lrintf(float x) {
 	return (long int)(x + 0.5f);
 }
-
-#endif // !defined(AFX_STDAFX_H__ECCA64B8_40F9_11D2_AD2F_00A0C9289E24__INCLUDED_)
-
