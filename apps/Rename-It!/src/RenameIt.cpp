99c5b07ac788d660be8d3ee9c14f9877cdd209e7
Werner Beroux <werner@beroux.com>
2012-08-12 01:45:04 +0100

    Remove the beta time bomb.
    The time bomb was to avoid people using an old beta version for a long
    period of time but it doesn't make much sense anymore.
    
    Change-Id: I0f238661536c5d0fe7c7ee438e2cf8db192ef10d

--- apps/Rename-It!/src/RenameIt.cpp
+++ apps/Rename-It!/src/RenameIt.cpp
@@ -62,122 +62,6 @@ CRenameItApp theApp;
 /////////////////////////////////////////////////////////////////////////////
 // CRenameItApp initialization
 
-// Return unsigned date of a specific day.
-// Note: Using #define to generate a single number in the generated binary.
-// \param year	Year [1900-inf].
-// \param month	Month [1-12].
-// \param day	Day of the month [1-31].
-#define DATE_AT(year, month, day) (unsigned)((year - 1771)*10000 + (month - 1)*100 + (day - 1))
-
-unsigned GetSafeCurrentDate()
-{
-	unsigned nCurrentDate = 0;
-
-	{// Using time().
-		_tzset();
-		time_t ltime;
-		time(&ltime);
-
-		tm today;
-		_localtime64_s(&today, &ltime);
-		nCurrentDate = max(nCurrentDate, DATE_AT(today.tm_year + 1900, today.tm_mon + 1, today.tm_mday));
-	}
-
-	{// Using a file.
-		// A helper class to get the path to some specific files in the system.
-		class CSystemPath
-		{
-		public:
-			enum ESystemPath
-			{
-				sysNone,
-				sysSystem32,
-				sysProgramFiles,
-				sysWindows,
-			};
-
-			CSystemPath() :
-				m_nPath(sysNone)
-			{
-			}
-
-			CSystemPath(ESystemPath nPath, CString strFileName) :
-				m_nPath(nPath),
-				m_strFileName(strFileName)
-			{
-			}
-
-			CString GetFullPath() const
-			{
-				CString strPath;
-
-				switch (m_nPath)
-				{
-				case sysNone:
-					break;
-
-				case sysSystem32:
-					if (SUCCEEDED(::SHGetFolderPath(NULL, CSIDL_SYSTEM, NULL, SHGFP_TYPE_CURRENT, strPath.GetBuffer(MAX_PATH))))
-						strPath.ReleaseBuffer();
-					else
-						strPath.ReleaseBuffer(0);
-					break;
-
-				case sysProgramFiles:
-					if (SUCCEEDED(::SHGetFolderPath(NULL, CSIDL_PROGRAM_FILES, NULL, SHGFP_TYPE_CURRENT, strPath.GetBuffer(MAX_PATH))))
-						strPath.ReleaseBuffer();
-					else
-						strPath.ReleaseBuffer(0);
-					break;
-
-				case sysWindows:
-					if (SUCCEEDED(::SHGetFolderPath(NULL, CSIDL_WINDOWS, NULL, SHGFP_TYPE_CURRENT, strPath.GetBuffer(MAX_PATH))))
-						strPath.ReleaseBuffer();
-					else
-						strPath.ReleaseBuffer(0);
-					break;
-					
-				default:
-					ASSERT(false);
-				}
-
-				::PathAppend(strPath.GetBufferSetLength(MAX_PATH), m_strFileName);
-				strPath.ReleaseBuffer();
-
-				return strPath;
-			}
-
-		private:
-			ESystemPath m_nPath;
-			CString m_strFileName;
-		};
-
-		vector<CSystemPath> vPathList;
-		vPathList.push_back(CSystemPath(CSystemPath::sysSystem32, _T("comdlg32.ocx")));
-		vPathList.push_back(CSystemPath(CSystemPath::sysProgramFiles, _T("")));
-		vPathList.push_back(CSystemPath(CSystemPath::sysWindows, _T("..\\pagefile.sys")));
-
-		BOOST_FOREACH(const CSystemPath& systemPath, vPathList)
-		{
-			// Try to get the file/folder time.
-			CFileFind ff;
-			if (ff.FindFile(systemPath.GetFullPath()))
-			{
-				ff.FindNextFile();
-
-				CTime time;
-				ff.GetLastAccessTime(time);
-				nCurrentDate = max(nCurrentDate, DATE_AT(time.GetYear(), time.GetMonth(), time.GetDay()));
-
-				ff.Close();
-			}
-		}
-	}
-
-	// Return the latest date found.
-	return nCurrentDate;
-}
-
 static void SetupExceptionHandler()
 {
 	// Initialize BugTrap
@@ -198,21 +82,10 @@ BOOL CRenameItApp::InitInstance()
 	// Global unhandled exception catcher.
 	SetupExceptionHandler();
 
-	// Beta time bomb.
-	if (GetSafeCurrentDate() > DATE_AT(2010, 12, 12))
-	{
-		// Show time bomb expired dialog.
- 		CAboutDlg dlg;
- 		m_pMainWnd = &dlg;
- 		dlg.DoModal();
-	}
-	else
-	{
-		// Show main dialog
-		CRenameItDlg	dlg;
-		m_pMainWnd = &dlg;
-		dlg.DoModal();
-	}
+	// Show main dialog
+	CRenameItDlg	dlg;
+	m_pMainWnd = &dlg;
+	dlg.DoModal();
 
     // Since the dialog has been closed, return FALSE so that we exit the
     //  application, rather than start the application's message pump.
